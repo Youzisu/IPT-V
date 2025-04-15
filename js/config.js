cc80b9084be47d89acabccb6ae3a11004810d9b7
Youzisu <kiss1298219974@outlook.com>
2025-04-16 06:19:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -195,5 +195,5 @@ const CUSTOM_API_CONFIG = {
     adultPropName: 'isAdult'  // 用于标记成人内容的属性名
 };
 
-// 新增隐藏内置黄色采集站API的变量，默认为true
-const HIDE_BUILTIN_ADULT_APIS = true;
+// 新增隐藏内置黄色采集站API的变量，默认为true，可开启false
+const HIDE_BUILTIN_ADULT_APIS = false;
